69f8eb2e697b4878fc274fbb123987d3097ac71f
Denis Soke <sokedenis@gmail.com>
2025-03-26 18:43:09 +0100

    Defined task model

--- model.go
+++ model.go
@@ -1,2 +1,17 @@
-// Task model(databse structure)
+/*
+Task model - Structure of Task object to store in SQLite database
+gorm.model - Predefined struct, to include fields like ID, CreatedAt, UpdatedAt, DeletedAt
+Fields - Title of the task, description of the task and boolean to check if task is completed or not
+*/
 package main
+
+import (
+	"gorm.io/gorm"
+)
+
+type Task struct{
+	gorm.Model
+		Title string 
+		Description string
+		IsCompleted bool
+	}
