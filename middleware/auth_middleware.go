df18610a52089b00d5cf6f0bd1848d682d975a93
Denis Soke <sokedenis@gmail.com>
2025-03-29 13:15:11 +0100

    Update middlewareu

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -1,2 +1,47 @@
-// Auth middleware( extracts user ID & role )
+/*
+Auth middleware
+Creating middleware to check if a user is aut.
+Checks if the request has a valid JWT token and extract user_id from the token.
+*/
 package middleware
+
+import (
+	"TaskManagerAPI/utils"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
+			c.Abort()
+			return
+		}
+
+		tokenStr := strings.Split(authHeader, "Bearer ")
+		if len(tokenStr) < 2 {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token format"})
+			c.Abort()
+			return
+		}
+
+		token, err := jwt.Parse(tokenStr[1], func(token *jwt.Token) (interface{}, error) {
+			return utils.JwtKey, nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			c.Abort()
+			return
+		}
+
+		claims, _ := token.Claims.(jwt.MapClaims)
+		c.Set("user_id", uint(claims["user_id"].(float64)))
+		c.Next()
+	}
+}
