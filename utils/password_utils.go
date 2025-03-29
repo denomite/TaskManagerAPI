e265214d1398a81ecd3c9533942343e6a5171277
Denis Soke <sokedenis@gmail.com>
2025-03-29 12:48:11 +0100

    Set up JWT

--- utils/password_utils.go
+++ utils/password_utils.go
@@ -1,2 +1,17 @@
-// Password hashing & verification
+/*
+Password hashing & verification
+Authentication so user can log and get a token
+*/
 package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
