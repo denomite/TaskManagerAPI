282f544d43c06ad1506c5a31157f99b5d47a487f
Denis Soke <sokedenis@gmail.com>
2025-03-29 12:48:11 +0100

    Set up JWT

--- utils/jwt_utils.go
+++ utils/jwt_utils.go
@@ -1,2 +1,24 @@
-// JWT Token generation & validation
+/*
+JWT Token generation & validation
+Function to create JWT tokens for users
+*/
 package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte("your_secret_key")
+
+func GenerateJWT(userID uint) (string, error) {
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	return token.SignedString(jwtKey)
+
+}
